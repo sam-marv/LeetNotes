ce176bcf47056ffa6f56677067bb234c0e0b2c14
SamuelPickering <samuelpickering21@gmail.com>
2024-01-16 23:48:25 -0500

    Leetcode adapter supports filtering

--- frontend/src/adapters/leetcode-adapter.js
+++ frontend/src/adapters/leetcode-adapter.js
@@ -2,9 +2,13 @@ import { fetchHandler, getPostOptions, basicFetchOptions, getPatchOptions } from
 
 const baseUrl = '/api/leetcodes';
 
-export const getAllPages = async (offset) => {
-//     const appPageOptions =
-//   const [leetcodes] = await fetchHandler(baseUrl, );
+export const getLeetcodes = async (obj) => {
+    console.log(obj.offset)
+    let url = `/api/leetcodes?offset=${obj.offset}`
+    if(obj.difficulty !== null) url += `&difficulty=${obj.difficulty}`
+    if(obj.tag !== null) url += `&tag=${obj.tag}`
+  const [leetcodes] = await fetchHandler(url);
+  console.log(leetcodes)
   return leetcodes || [];
 };
 
