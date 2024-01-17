ce8a25a9621e2f93c02dc2c7d69bce08634cc723
SamuelPickering <samuelpickering21@gmail.com>
2024-01-16 23:47:42 -0500

    Adding functionality for filtering on tags and difficulty

--- src/controllers/leetcode/list50.js
+++ src/controllers/leetcode/list50.js
@@ -1,14 +1,16 @@
 const list50 = async (req, res) => {
     const {
       db: { Leetcode }, // this req.db.User property is put here by the addModelsToRequest middleware    THANK GOD THIS COMMENT WAS HERE
-      body: {offset}
+      query: { offset, difficulty, tag}
     } = req;
   
     console.log('hiiiii');
     console.log('yo');
   
-    const leetcodes = await Leetcode.get50(offset);
+    const leetcodes = await Leetcode.get50(offset, difficulty, tag);
     res.send(leetcodes);
   };
   
   module.exports = list50;
+
+  //
--- src/db/models/leetcode.js
+++ src/db/models/leetcode.js
@@ -1,11 +1,42 @@
 const knex = require('../knex');
 
 class Leetcode {
-  static async get50(offset) {
-    const query = 'SELECT * FROM leetcodes LIMIT 50 OFFSET ?';
-    const args = [offset]
-    const { rows } = await knex.raw(query, args);
-    return rows || [];
+  static async get50(offset, difficulty = null, tag = null) {
+  // Base query
+  let query = `
+    SELECT leetcodes.*, STRING_AGG(leettags.name, ',') as tagNames
+    FROM leetcodes
+    LEFT JOIN leettagleetcode ON leetcodes.leetcode_id = leettagleetcode.leetcode_id
+    LEFT JOIN leettags ON leettagleetcode.leettag_id = leettags.leettag_id
+  `;
+
+  // Conditionally add WHERE clause based on the presence of optional parameters
+  const conditions = [];
+  const args = [];
+
+  if (difficulty !== null) {
+    conditions.push('leetcodes.difficulty = ?');
+    args.push(difficulty);
+  }
+
+  if (tag !== null) {
+    conditions.push('leettags.name = ?');
+    args.push(tag);
+  }
+
+  if (conditions.length > 0) {
+    query += ' WHERE ' + conditions.join(' AND ');
+  }
+
+  // Im def adding more comments tommorow this was a bitch, gn
+  query += ' GROUP BY leetcodes.leetcode_id';
+
+  // Add LIMIT and OFFSET to the query
+  query += ' LIMIT 50 OFFSET ?';
+  args.push(offset);
+
+  const { rows } = await knex.raw(query, args);
+  return rows || [];
   }
 
 
